e250ab57f4da3e5f6ff3c2dd750939eb0397dc99
Sasha Alimov <aalimov@wiley.com>
2021-02-12 13:38:11 +0300

    Add API call and CSS

---
+++ site/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!--  -->
+  <div id="App">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Neon />
+    <Table />
+    <HelloWorld />
+    
+  </div>
+
+</template>
+
+<script>
+import Table from './components/Table.vue'
+import Neon from './components/Neon.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Neon,
+    Table,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+body {
+  background-color: black;
+  color: #ffffff;
+  }
+</style>
