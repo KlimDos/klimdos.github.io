370b503ad1ec082aa0ee1631b03569c0a3e9f6b6
Sasha Alimov <aalimov@wiley.com>
2021-02-12 18:09:20 +0300

    Add ya metrics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// your main.js
+import VueYandexMetrika from 'vue-yandex-metrika'                               
+
+Vue.use(VueYandexMetrika, {
+    id: 72339736,
+    env: "production"
+    // other options
+})
